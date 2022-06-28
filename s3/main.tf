d851c40f6daa5a8e2b39b8d9cf0ef69575179f09
vinod9030 <108118167+vinod9030@users.noreply.github.com>
2022-06-29 00:59:08 +0530

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -1,14 +1,12 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_s3_bucket" "money-onTime-V1" {
-  bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+  tags = var.tags
    versioning {
     enabled = var.versioning
   }
-  
-  tags = var.tags
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = var.acl
 }
